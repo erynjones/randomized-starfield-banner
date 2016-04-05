3a192b86e961522ed52fb3b76527a1acb24ced78
erynjones <eryn.jones16@hotmail.com>
2016-04-05 08:14:45 -0400

    starting basic animation

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var $banner = $('.banner');
+var $addStars;
+var $width = $banner.outerWidth();
+var $height = $banner.outerHeight();
+
+for (i = 0; i < 75; i++) {
+  $rotateNum = Math.random() * 1000;
+  $scaleNum = Math.random() * 10000;
+  $animateNum = Math.random() * 50;
+  $addStars = $('<div>');
+  $addStars.addClass('star');
+  $addStars.css({
+    left: Math.random() * 2000,
+    top: Math.random() * 2000,
+    opacity: Math.random() * 10,
+    width: Math.random() * 100,
+    transform: 'rotate'( + $rotateNum + 'deg'),
+    transform: 'scale'( + $scaleNum + '%'),
+    animation: 'growstars' + $animateNum + 'alternate infinite'
+  });
+
+  $banner.append($addStars);
+};
