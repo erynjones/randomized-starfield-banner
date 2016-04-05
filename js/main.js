c11f29c971caae4932f57ffb4f40b208540f4bbc
erynjones <eryn.jones16@hotmail.com>
2016-04-05 08:18:34 -0400

    changed animation

--- js/main.js
+++ js/main.js
@@ -3,21 +3,19 @@ var $addStars;
 var $width = $banner.outerWidth();
 var $height = $banner.outerHeight();
 
-for (i = 0; i < 75; i++) {
-  $rotateNum = Math.random() * 1000;
-  $scaleNum = Math.random() * 10000;
-  $animateNum = Math.random() * 50;
-  $addStars = $('<div>');
-  $addStars.addClass('star');
-  $addStars.css({
-    left: Math.random() * 2000,
-    top: Math.random() * 2000,
-    opacity: Math.random() * 10,
-    width: Math.random() * 100,
-    transform: 'rotate'( + $rotateNum + 'deg'),
-    transform: 'scale'( + $scaleNum + '%'),
-    animation: 'growstars' + $animateNum + 'alternate infinite'
-  });
+var $body = $('.banner');
+var $star;
+var width = $body.outerWidth();
+var height = $body.outerHeight();
 
-  $banner.append($addStars);
-};
+for (i = 0; i < 75; i++) {
+  $star = $('<div>');
+  $star.addClass('star');
+  $star.css({
+    top: Math.random() * height,
+    left: Math.random() * width,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
+  });
+  $body.append($star);
+}
